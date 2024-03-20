e594599ec990bcd60a961a11d069ea3a7183ed27
Akshitha <akshithacheruku23@gmail.com>
2024-03-20 23:38:27 +0530

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>DSA Enthusiast</h3>
+              <p>
+                Love to solve algorithmic problems. Solved 200+ problems on leetcode.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages using Figma.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Google Talentsprint WE Scholar</h3>
+              <p>
+                Selected to be a part of a community of 300 women engineers chosen from all over India.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>GEEKSFORGEEKS</h3>
+              <p>
+                Won Silver Medal for securing top 150
+                of Mega Jon-A-Thon for freshers.
+              </p>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+    </section>
+  );
+};
